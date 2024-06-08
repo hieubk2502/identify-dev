f7e9db89340144834149ee09fd684c51f6f9b898
hieubk2502 <diemdungodau2502@gmail.com>
2024-06-09 00:19:14 +0700

    CRUD basic

---
+++ src/main/java/com/dev/identify/dev/entity/User.java
@@ -0,0 +1,37 @@
+package com.dev.identify.dev.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    String username;
+
+    String password;
+
+    String firstname;
+
+    String lastname;
+
+    LocalDate dob;
+
+}
--- src/test/java/com/dev/identify/dev/ApplicationTests.java
+++ src/test/java/com/dev/identify/dev/ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.dev.identify.dev;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.dev.identify.dev;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
