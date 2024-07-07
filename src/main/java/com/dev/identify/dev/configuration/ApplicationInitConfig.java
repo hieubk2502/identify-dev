8c7553a9553311c5ef47ac810d91051d74d81deb
hieubk2502 <diemdungodau2502@gmail.com>
2024-07-07 13:48:43 +0700

    add unit test

--- src/main/java/com/dev/identify/dev/configuration/ApplicationInitConfig.java
+++ src/main/java/com/dev/identify/dev/configuration/ApplicationInitConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,8 +24,11 @@ public class ApplicationInitConfig {
 
     PasswordEncoder passwordEncoder;
 
+
+    @ConditionalOnProperty(value = "spring.datasource.url", havingValue = "jdbc:mysql://localhost:3306/identify")
     @Bean
     ApplicationRunner applicationRunner(UserRepository userRepository) {
+        log.info("Init Application config .............");
         return args -> {
 
             boolean userAdminExist = userRepository.existsByUsername("admin");
--- src/main/java/com/dev/identify/dev/dto/request/UserCreateRequest.java
+++ src/main/java/com/dev/identify/dev/dto/request/UserCreateRequest.java
@@ -2,9 +2,7 @@ package com.dev.identify.dev.dto.request;
 
 import com.dev.identify.dev.validator.DobConstraint;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
@@ -12,6 +10,8 @@ import java.time.LocalDate;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserCreateRequest {
 
     @Size(min = 4, message = "USERNAME_INVALID")
